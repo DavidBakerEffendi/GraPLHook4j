83be2414c359ec4b352c36362bf06bdbc58fa585
David Baker Effendi <davidbakereffendi@hotmail.com>
2020-02-26 17:19:36 +0200

    FIX: Fixed Log4j 2 problems

--- src/main/java/za/ac/sun/grapl/domain/models/vertices/ModifierVertex.java
+++ src/main/java/za/ac/sun/grapl/domain/models/vertices/ModifierVertex.java
@@ -1,6 +1,6 @@
 package za.ac.sun.grapl.domain.models.vertices;
 
-import za.ac.sun.grapl.domain.enums.EvaluationStrategies;
+import za.ac.sun.grapl.domain.enums.ModifierTypes;
 import za.ac.sun.grapl.domain.enums.VertexBaseTraits;
 import za.ac.sun.grapl.domain.enums.VertexLabels;
 import za.ac.sun.grapl.domain.models.GraPLVertex;
@@ -14,15 +14,11 @@ public class ModifierVertex implements GraPLVertex {
     public static final VertexLabels LABEL = VertexLabels.MODIFIER;
     public static final EnumSet<VertexBaseTraits> TRAITS = EnumSet.of(VertexBaseTraits.AST_NODE);
 
-    public final EvaluationStrategies evaluationStrategy;
-    public final String typeFullName;
-    public final int lineNumber;
+    public final ModifierTypes modifierType;
     public final int order;
 
-    public ModifierVertex(EvaluationStrategies evaluationStrategy, String typeFullName, int lineNumber, int order) {
-        this.evaluationStrategy = evaluationStrategy;
-        this.typeFullName = typeFullName;
-        this.lineNumber = lineNumber;
+    public ModifierVertex(ModifierTypes modifierType, int order) {
+        this.modifierType = modifierType;
         this.order = order;
     }
 }
